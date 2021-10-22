2b1df3e23b86e832950b5edc6a53d7489e24dbf2
MehulFPrimec <50493584+MehulFPrimec@users.noreply.github.com>
2021-10-22 11:14:01 +0530

    Intial Commit - Project creation

--- ARApp/ViewController.swift
+++ ARApp/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  ARApp
 //
-//  Created by FPrimeC on 2021-10-21.
+//  Created by FPrimeC on 2021-10-22.
 //
 
 import UIKit
@@ -11,9 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Physics AR"
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.barTintColor = UIColor.black
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        self.navigationController?.navigationBar.tintColor = UIColor(red: 211/255, green: 86/255, blue: 50/255, alpha: 1.0)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
